7cc4712c1e1da15e37f00e75b3801e5cb0e8564c
webtalent0125 <talenteddev0125@gmail.com>
2023-04-13 02:30:17 +0900

    Keep calm and commit

--- src/stories/organisms/Hero/Hero.tsx
+++ src/stories/organisms/Hero/Hero.tsx
@@ -31,20 +31,13 @@ const divStyle = {
   height: '100vh'
 } as React.CSSProperties;
 
-
 const Hero = () => {
 
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    await console.log(container);
   }, []);
 
   return (
